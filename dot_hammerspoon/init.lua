e43f5f12ec9bb77206af4f516b7677af4f78be5d
Nikola Milojević <nikola@milojevic.me>
2023-04-04 15:19:25 +0200

    remove guarana

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,7 +1,6 @@
 hs.loadSpoon("WindowHalfsAndThirds")
 
-require "modules/windowmgmt"
-require "modules/layout"
-require "modules/applauncher"
-require "modules/reloader"
-require "modules/guarana"
+require("modules/windowmgmt")
+require("modules/layout")
+require("modules/applauncher")
+require("modules/reloader")
