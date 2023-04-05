34a8e9c13f39798a169458565d1d522895cfc90a
Nikola Milojević <nikola@milojevic.me>
2023-04-05 20:05:42 +0200

    added mux remove

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -54,4 +54,9 @@ config.colors = {
 config.unix_domains = { { name = "unix" } }
 config.default_gui_startup_args = { "connect", "unix" }
 
+config.mux_env_remove = {
+	"SSH_CLIENT",
+	"SSH_CONNECTION",
+}
+
 return config
