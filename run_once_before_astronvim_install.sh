966fb541d96a4433c716b5e6da8eb4ef80cbaeff
Nikola Milojević <nikola@milojevic.me>
2023-04-13 16:42:20 +0200

    move hidden files as well

--- run_once_before_astronvim_install.sh
+++ run_once_before_astronvim_install.sh
@@ -5,6 +5,6 @@ git clone --depth 1 https://github.com/AstroNvim/AstroNvim /tmp/astronvim
 
 mkdir $HOME/.config/nvim
 
-mv /tmp/astronvim/* $HOME/.config/nvim
+mv /tmp/astronvim/{.,}* $HOME/.config/nvim
 
 echo "~ astronvim has been installed"
