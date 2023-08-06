9054c6f6e5bc967b860d12617f930de3107a9783
Nikola Milojević <nikola@milojevic.me>
2023-08-06 17:05:59 +0200

    switch from asdf to rtx

--- run_once_01_asdf_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [[ ! $(command -v asdf) ]]; then
-	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2 >/dev/null 2>&1 || true && echo "~ asdf has been installed"
-fi
---
+++ run_once_01_rtx_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://rtx.pub/install.sh | sh
