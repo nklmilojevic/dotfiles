aa01289acd26530e7ef1e3fbf2aaf741073607dd
Nikola Milojević <nikola@milojevic.me>
2024-02-13 11:36:06 +0100

    formatting

--- dot_hammerspoon/modules/applauncher.lua
+++ dot_hammerspoon/modules/applauncher.lua
@@ -5,17 +5,17 @@ local hyper = { "ctrl", "alt", "cmd", "shift" }
 spoon.AppLauncher.modifiers = hyper
 
 hs.spoons.use("AppLauncher", {
-    hotkeys = {
-        c = "Slack",
-        v = "Visual Studio Code",
-        n = "NotePlan",
-        m = "Mail",
-        z = "Messages",
-        f = "Brave Browser",
-        i = "IntelliJ IDEA",
-        r = "WezTerm",
-        s = "Spotify",
-        t = "Telegram",
-        d = "Discord",
-    },
+	hotkeys = {
+		c = "Slack",
+		v = "Visual Studio Code",
+		n = "NotePlan",
+		m = "Mail",
+		z = "Messages",
+		f = "Brave Browser",
+		i = "IntelliJ IDEA",
+		r = "WezTerm",
+		t = "Telegram",
+		d = "Discord",
+		s = "Spotify",
+	},
 })
