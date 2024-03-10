586be0e18a114c19bc3f6189c49e75efb6303a3b
Nikola Milojević <nikola@milojevic.me>
2024-03-10 21:32:28 +0100

    feat: starship is slow, use tide as it is faster and behaves more like p10k in zsh

---
+++ .chezmoiscripts/run_once_after_08_tide_configure.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+tide configure --auto --style='Lean' --prompt_colors='True color' --show_time='24-hour format' --prompt_spacing='Compact' --icons='Many icons' --transient='Yes' --lean_prompt_height='One line'
+
+echo "✅ tide theme is configured!"
